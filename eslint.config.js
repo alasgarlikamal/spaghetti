87d2e90b65eac76a554079e4d87ffdad71307dbe
Kamal <elesgerlikamal04@gmail.com>
2024-11-19 08:45:52 +0400

    chore: remove comments

--- eslint.config.js
+++ eslint.config.js
@@ -25,8 +25,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      ...vitest.configs.recommended.rules, // you can also use vitest.configs.all.rules to enable all rules
-      'vitest/max-nested-describe': ['error', { max: 3 }], // you can also modify rules' behavior using option like this
+      ...vitest.configs.recommended.rules,
     },
   },
 );
